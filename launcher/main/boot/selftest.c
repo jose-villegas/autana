061aa7a570d6ed227e5370b9752f03736c3d6e4f
Jose Villegas <villegasjose.gg@gmail.com>
2026-09-25 14:18:11 +0200

    tests: route Unity output to diagnostic console

--- launcher/main/boot/selftest.c
+++ launcher/main/boot/selftest.c
@@ -26,6 +26,11 @@
 
 static const char* TAG = "selftest";
 
+void
+__wrap_esp_system_console_put_char(char c) {
+    (void)putchar((unsigned char)c);
+}
+
 /* Unity requires these once per binary. The suites manage their own fixtures,
  * since they all share this program. */
 void
